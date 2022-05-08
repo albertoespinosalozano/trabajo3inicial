555fed50b63540e35658b69e38263c2c26a38137
albertoespinosalozano <60694491+albertoespinosalozano@users.noreply.github.com>
2022-05-08 05:27:46 +0200

    Add files via upload

---
+++ src/main/java/com/practica/genericas/Documentacion.java
@@ -0,0 +1,20 @@
+package com.practica.genericas;
+
+public class Documentacion {
+	private String documento;
+	
+	public Documentacion() {
+		
+	}
+	public Documentacion(String documento) {
+		this.documento = documento;
+	}
+	
+	public String getDocumento() {
+		return documento;
+	}
+
+	public void setDocumento(String documento) {
+		this.documento = documento;
+	}
+}
